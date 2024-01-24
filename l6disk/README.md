aff06fde9efb40b852356345919dd2357f89c66d
Sergio Carmine <me@sergiocarmi.net>
2024-01-25 00:00:18 +0100

    l6disk: implement basic arg parsing Create basic folder structure Implement rudimentray argument parsing

---
+++ l6disk/README.md
@@ -0,0 +1,3 @@
+# Level 6 diskette image utility
+
+Simple utility that facilitates the creation and reading of Level6-formatted raw floppy images
---
+++ l6disk/src/args.rs
@@ -0,0 +1,21 @@
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+
+#[derive(Debug, Parser)]
+#[command(version, about)]
+pub struct Args {
+    #[clap(subcommand)]
+    pub command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Command {
+    /// Create level6 encoded floppy image
+    Create {
+        /// Input disk image
+        input: PathBuf,
+
+        /// Input disk image
+        output: PathBuf,
+    },
+}
